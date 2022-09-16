1e2f5dbccfcd10cf275963ee050e59e2998249b4
Afsal p u <afsalpu16@gmail.com>
2022-09-16 21:51:37 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '430352',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Antenna for vehicle',
+        productNumber: '431352',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'NIVIA football',
+        productNumber: '433352',
+        paymentStatus: 'Paid',
+        shipping: 'Delevered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '430352',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '430352',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
